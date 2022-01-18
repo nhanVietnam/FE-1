85d509a2741ec702127c1c1220de99c077c7cd72
nhanVietnam <62474480+nhanVietnam@users.noreply.github.com>
2022-01-18 12:30:03 +0700

    Second

--- js/main.js
+++ js/main.js
@@ -38,3 +38,34 @@ const swiper = new Swiper('.swiper', {
         },
 }
 });
+
+const menu = document.getElementById("menu")
+const navigation = document.getElementsByClassName("header__mobile__modal")
+const close = document.getElementById("close-navigation")
+menu.addEventListener("click",function() {
+    navigation[0].classList.add("is-show")
+    close.classList.add("is-show")
+})
+
+close.addEventListener("click",function() {
+    document.getElementById("navigation").classList.remove("is-show")
+    close.classList.remove("is-show")
+})
+
+
+const mobile_submenu = Array.from(document.querySelectorAll(".header__mobile__item"));
+mobile_submenu.forEach((element) => {
+    const submenu = element.querySelector(".header__mobile__submenu");
+    if (!submenu) { return; }
+    element.addEventListener("pointerenter", (e) => {
+        submenu.style.height = "auto";
+        let height = submenu.offsetHeight;
+        submenu.style.height = "0px";
+        setTimeout(() => {
+            submenu.style.height = `${height}px`;
+        }, 16);
+    });
+    element.addEventListener("pointerleave", (e) => {
+        submenu.style.height = null;
+    });
+});
